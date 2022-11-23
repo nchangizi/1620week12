b8b2f78412fdc0183539edcee57bd5f6d5a50f7c
Neda <nedachangizi@gmail.com>
2022-11-23 14:05:46 -0800

    button bg

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,48 @@
+// import log from '../test'
+// log();
 const clickMeButton = document.querySelector(".click-me");
 // console.log(clickMeButton);
 
-function clickHandler ()
-{
-    alert("You clicked me!");
-    // clickMeButton.removeEventListener("click", clickHandler)
+function clickHandler() {
+  alert("You clicked me!");
+  // clickMeButton.removeEventListener("click", clickHandler)
 }
 
-clickMeButton.addEventListener("click", clickHandler,{once:true});
+clickMeButton.addEventListener("click", clickHandler, { once: true });
 
-function changeBGPink()
-{
-    // document.body.style.backgroundColor = "pink";
-    
-    document.body.classList.add("pink-bg");
+function changeBGPink() {
+  // document.body.style.backgroundColor = "pink";
+
+  document.body.classList.add("pink-bg");
 }
 clickMeButton.addEventListener("click", changeBGPink);
 
-function changeText()
-{
-    if( clickMeButton.textContent === 'Click Me!' )
-    {
-        clickMeButton.textContent = "clicked";
-    }
-    else if (clickMeButton.textContent === "clicked")
-    {
-        clickMeButton.textContent = "Click Me!";
-    }
+function changeText() {
+  if (clickMeButton.textContent === "Click Me!") {
+    clickMeButton.textContent = "clicked";
+  } else if (clickMeButton.textContent === "clicked") {
+    clickMeButton.textContent = "Click Me!";
+  }
 }
 clickMeButton.addEventListener("click", changeText);
 
-function updateImage()
-{
-    const image = document.querySelector("#shoppingCart");
-    // console.log(image);
-    image.src = "images/263142.png";
-    image.width=100;
-    image.height=100;
+function updateImage(event) {
+  // console.log(event.target.textContent);
+  const image = document.querySelector("#shoppingCart");
+  // console.log(image);
+  image.src = "images/263142.png";
+  image.width = 100;
+  image.height = 100;
+  image.alt = "shopping cart icon";
 }
 clickMeButton.addEventListener("click", updateImage);
+
+const buttonContainer = document.querySelector(".button-container");
+
+function changeBGGreen(e) {
+  if (e.target.tagName === "BUTTON") {
+    // e.target.style.backgroundColor = "green";
+    e.target.classList.add("greenBG");
+  }
+}
+buttonContainer.addEventListener("mouseover", changeBGGreen);
